69cf1b42ec19ae56ffc1293a6454c60f436556a8
Muhammad Shofa <mshofa09@gmail.com>
2024-03-14 10:45:21 +0700

    today i learn definition of inheritance OOP

---
+++ inheritance.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+inheritance atau pewarisan sifat merupakan class turunan dari class lainnya,
+inheritance dibuat dengan membuat class baru dengan tujuan dihubungkan dengan class yang lain yang di inginkan,
+dengan tujuan untuk mendpaatkan informasi atau data dari class yang dihubungkan tersebut.
+
+*/ 
+
+
+?>
