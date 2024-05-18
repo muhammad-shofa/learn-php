a523419b6c56b6df6abf3d305f4a670cd74b458c
Muhammad Shofa <mshofa09@gmail.com>
2024-05-18 09:32:20 +0700

    today I learned how to use callback functions in jquery

--- jquery-ajax/jquery-basic/index.php
+++ jquery-ajax/jquery-basic/index.php
@@ -12,6 +12,13 @@
     <h1>Belajar JQuery</h1>
     <button id="button">Button</button>
     <div class="box"></div>
+    <br>
+    <h2>Callback Function</h2>
+    <button id="button-callback">Button Callback</button>
+    <div class="box-1"></div>
+    <div class="box-callback">
+        <p>Callback Function</p>
+    </div>
     <!-- JQuery -->
     <script src="assets/js/jquery-3.7.1.min.js"></script>
     <script>
@@ -19,7 +26,15 @@
             $("#button").click(function () {
                 $(".box").toggle();
             })
-        })
+        });
+
+        $(document).ready(function () {
+            $("#button-callback").click(function () {
+                $(".box-1").fadeIn(800, function () {
+                    $(".box-callback").show();
+                })
+            })
+        });
     </script>
 </body>
 
